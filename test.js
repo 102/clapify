9a83b9fc7c824dbe1f64beeee48580fabd91d362
Roman Gusev <rgusev2@gmail.com>
2020-01-17 00:31:03 +0600

    Update test suite

--- test.js
+++ test.js
@@ -1,14 +1,35 @@
 const assert = require("assert");
 const clapify = require(".");
 
-assert.equal(clapify("test"), "test");
-assert.equal(clapify("test test"), "test👏test");
-assert.equal(
-  clapify(
+const testEqual = (message, a, b) => {
+  try {
+    assert.equal(a, b);
+    console.log(`PASS ${message}`);
+  } catch (error) {
+    console.error(`FAIL ${message} with values "%s" "%s"`, a, b);
+    process.exitCode = 1;
+  }
+};
+
+[
+  ["does not modify text without space symbols", "test", "test"],
+  ["replaces single space with clap emoji", "test test", "test👏test"],
+  [
+    "replaces newline with clap emoji",
     `test
-test`
-  ),
-  "test👏test"
-);
-assert.equal(clapify("test test  test"), "test👏test👏test");
-assert.equal(clapify("this is not a joke"), "this👏is👏not👏a👏joke");
+test`,
+    "test👏test"
+  ],
+  [
+    "replaces multiple spaces in text with a single clap emoji",
+    "test test  test",
+    "test👏test👏test"
+  ],
+  [
+    "works as expected with example from readme",
+    "this is not a joke",
+    "this👏is👏not👏a👏joke"
+  ]
+].forEach(([message, text, expected]) => {
+  testEqual(message, clapify(text), expected);
+});
